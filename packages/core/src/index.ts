f131215000a90d4603b66375ffe2a43f12cac9bf
Geonhyuk Lee <geonhyeoklee.kr@gmail.com>
2025-12-16 18:31:00 +0900

    feat(core): add ShaderMaterial type export to material module

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -35,6 +35,7 @@ export {
   type ParallaxMaterialOptions,
   type ParallaxPBRMaterialOptions,
   type PBRMaterialOptions,
+  type ShaderMaterialOptions,
   BasicMaterial,
   BlinnPhongMaterial,
   VertexColorMaterial,
@@ -44,6 +45,7 @@ export {
   ParallaxMaterial,
   ParallaxPBRMaterial,
   PBRMaterial,
+  ShaderMaterial,
 } from "./material";
 export { ShaderLib, type ShaderSource } from "./shaders";
 export * from "./camera";
